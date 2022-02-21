1b03084b292ebee321b0650c00d111078e9e4914
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-02-21 20:12:58 -0300

    Curso VueJS - parte [3/7]

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <!-- <h1 v-text="titulo"></h1> -->
+	<ul>
+		<li v-for="item of fotos">
+			<img :src="item.url" v-bind:alt="item.titulo">
+		</li>
+	</ul>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			titulo: "Alurapic",
+			fotos: []
+		}
+	},
+
+	created() {
+		let promisse = this.$http.get("http://localhost:3000/v1/fotos");
+		promisse.then(res => {
+			res.json().then(fotos => this.fotos = fotos);
+		});
+	}
+}
+</script>
+
+<style>
+</style>
