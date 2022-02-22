c9665ba4b5d77099551a675e457ea12bdc44b750
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-02-21 21:33:20 -0300

    Criando components e encapsulando views

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
     <!-- <h1 v-text="titulo"></h1> -->
-	<ul>
-		<li v-for="item of fotos">
-			<img :src="item.url" v-bind:alt="item.titulo">
+	<ul class="lista-fotos">
+		<li class="lista-fotos-item" v-for="foto of fotos">
+			<meu-painel :titulo="foto.titulo">
+				<img class="imagem-responsiva" :src="foto.url" v-bind:alt="foto.titulo">
+			</meu-painel>
 		</li>
 	</ul>
   </div>
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+	components: {
+		"meu-painel": Painel
+	},
+
 	data() {
 		return {
 			titulo: "Alurapic",
@@ -29,4 +39,48 @@ export default {
 </script>
 
 <style>
+	.corpo {
+		font-family: Helvetica, sans-serif;
+		width: 96%;
+		margin: 0 auto;
+	}
+
+	.centralizado {
+		text-align: center;
+	}
+
+	.lista-fotos {
+		list-style: none;
+	}
+
+	.lista-fotos .lista-fotos-item {
+		display: inline-block;
+	}
+
+	.imagem-responsiva {
+		width: 100%;
+	}
+
+	/* estilo do painel */ 
+
+   .painel {
+	   padding: 0 auto;
+       border: solid 2px grey;
+       display: inline-block;
+       margin: 5px;
+       box-shadow: 5px 5px 10px grey;
+       width: 200px;
+       height: 100%;
+       vertical-align: top;
+       text-align: center;
+   }
+
+  .painel .painel-titulo {
+	  text-align: center;
+	  border: solid 2px;
+	  background: lightblue;
+	  margin: 0 0 15px 0;
+	  padding: 10px;
+      text-transform: uppercase;
+   }
 </style>
