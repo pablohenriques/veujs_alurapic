9067f73e5ef8141e0955899a587f2605eb952101
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-02-24 22:18:56 -0300

    Componente menu configurado

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="corpo">
-	  <nav>
-		  <ul>
-			  <li v-for="route in routes">
-				  <router-link :to="route.path ? route.path : '/'">
-					  {{ route.titulo }}
-				  </router-link>
-			  </li>
-		  </ul>
-	  </nav>
-	  <router-view></router-view>
+	  <meu-menu :rotas="routes"/>
+	  <transition name="pagina">
+	  	<router-view></router-view>
+	  </transition>
   </div>
 </template>
 
 <script>
 
 import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
 
 export default {
+	components: {
+		"meu-menu": Menu
+	},
 	data() {
 		return {
 			routes: routes,
@@ -33,4 +31,12 @@ export default {
 		width: 96%;
 		margin: 0 auto;
 	}
+
+	.pagina-enter, .pagina-leave-active {
+        opacity: 0;
+    }
+
+    .pagina-enter-active, .pagina-leave-active {
+        transition: opacity .4s;
+    }
 </style>
