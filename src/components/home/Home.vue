c75b1dae54c48f3a8e9d223f7155826eedf10bae
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-02-28 22:29:16 -0300

    Mais sobre diretivas

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -6,7 +6,7 @@
 	<ul class="lista-fotos">
 		<li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 			<meu-painel :titulo="foto.titulo">
-				<imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform.animate="15"/>
+				<imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform:scale.animate="1.2"/>
 				<meu-botao 
 					tipo="button" 
 					rotulo="REMOVER" 
