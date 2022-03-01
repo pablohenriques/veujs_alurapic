9d65cce68be7ca3811c1f7d8fe5dacc817eb3a66
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-02-28 21:29:07 -0300

    Adicionando diretivas

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -6,7 +6,7 @@
 	<ul class="lista-fotos">
 		<li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 			<meu-painel :titulo="foto.titulo">
-				<imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+				<imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform="15"/>
 				<meu-botao 
 					tipo="button" 
 					rotulo="REMOVER" 
