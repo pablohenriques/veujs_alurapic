c8e594f2075cdb0759126f0e54f66b248745fcdc
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-02-28 21:36:54 -0300

    Diretivas e parametros

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -6,7 +6,7 @@
 	<ul class="lista-fotos">
 		<li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 			<meu-painel :titulo="foto.titulo">
-				<imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform="15"/>
+				<imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform="{incremento: 15, animate: true}"/>
 				<meu-botao 
 					tipo="button" 
 					rotulo="REMOVER" 
--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -3,12 +3,24 @@ import Vue from 'vue';
 Vue.directive('meu-transform', {
 
     bind(el, binding, vnode) {
+
       let current = 0;
+
       el.addEventListener('dblclick', function() {
-        let incremento = binding.value || 90;
+
+        let incremento = 90;
+        let animate = false;
+
+        if(binding.value) {
+            incremento = binding.value.incremento
+            animate = binding.value.animate 
+
+        }
+
         current+=incremento;
-        el.style.transition = "transform 0.5s";
         this.style.transform = `rotate(${current}deg)`;
+        if (animate) this.style.transition = "transform 0.5s";
+
       });
     }
 
