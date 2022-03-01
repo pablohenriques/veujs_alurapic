57a59f82ba51efca96b6897877afe0a95943a136
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-03-01 16:45:42 -0300

    v-model adicionado

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -9,33 +9,18 @@
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input
-			id="titulo" 
-			autocomplete="off" 
-			@input="foto.titulo = $event.target.value"
-			:value="foto.titulo"
-		>
+        <input id="titulo" autocomplete="off" v-model="foto.titulo">
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input 
-			id="url" 
-			autocomplete="off" 
-			@input="foto.url = $event.target.value"
-			:value="foto.url"
-		>
-        <imagem-responsiva/>
+        <input id="url" autocomplete="off" v-model.lazy="foto.url">
+        <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
       </div>
 
       <div class="controle">
         <label for="descricao">DESCRIÇÃO</label>
-        <textarea 
-			id="descricao" 
-			autocomplete="off" 
-			@input="foto.descricao = $event.target.value"
-			:value="foto.descricao"
-		></textarea>
+        <textarea id="descricao" autocomplete="off" v-model="foto.descricao"></textarea>
       </div>
 
       <div class="centralizado">
@@ -51,6 +36,7 @@
 
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
 import Botao from '../shared/botao/Botao.vue';
+import Foto from "../../domain/foto/Foto";
 
 export default {
 
@@ -62,14 +48,14 @@ export default {
 
 	data() {
 		return {
-			foto:{titulo: "", url: "", descricao: ""}
+			foto: new Foto(),
 		}
 	},
 
 	methods: {
 		grava() {
-			console.log("Enviar dados para API");
-			this.foto = {titulo: " ", url: " ", descricao: " "}
+			this.$http.post("http://localhost:3000/v1/fotos", this.foto)
+			.then(() => this.foto = new Foto(), err => console.log(err));
 		}
 	}
 
